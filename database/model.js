9c8201ac2895472574c5c9f0ac47f2cd0a67a8b6
Hoon Park <hoonpark@Hoons-MacBook-Pro.local>
2022-03-23 19:50:12 -0700

    backend and database changes
    co-authored-by Sung Kim <sungkm26@gmail.com>
    co-authored-by Bryan Santos <brymsantos@gmail.com>

--- database/model.js
+++ database/model.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 
-const PG_URI = 'postgres://nrcjerbs:OV_sIkJx1POAnpR_HPeuqY9kaxnEbrkz@kashin.db.elephantsql.com/nrcjerbs';
+//const PG_URI = 'postgres://nrcjerbs:OV_sIkJx1POAnpR_HPeuqY9kaxnEbrkz@kashin.db.elephantsql.com/nrcjerbs';
+const PG_URI = 'postgres://vfwuhwjy:gW1WGjash3Z0KFCEsCQydgeAy5ey3-BC@kashin.db.elephantsql.com/vfwuhwjy';
 
 const pool = new Pool({
     connectionString: PG_URI
---
+++ server/controllers/dateController.js
@@ -0,0 +1,7 @@
+const { query } = require("express");
+const db = require("../../database/model");
+
+// create a date controller 
+    // query the database and insert into table
+
+module.exports = DateController
